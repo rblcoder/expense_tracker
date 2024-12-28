c8330330f31ab9ab6c035bff9478d6fc28b03744
Rupa Lahiri <35212670+rblcoder@users.noreply.github.com>
2024-12-28 13:12:35 +0000

    Add terraform code to deploy to ec2 and connect to rds db

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "terraform-state" # create your own bucket
+    bucket         = "yourbucket" # create your own bucket
     key            = "expenses/terraform.tfstate"
     region         = "us-west-2"
     
@@ -16,7 +16,23 @@ terraform {
 }
 
 
+variable "db_username" {
+  description = "Username for the RDS database"
+  type        = string
+  default     = "dbuser"
+}
 
+variable "db_password" {
+  description = "Password for the RDS database"
+  type        = string
+  default     = "yourpassword"
+}
+
+variable "db_name" {
+  description = "Name of the database"
+  type        = string
+  default     = "expensedb"
+}
 
 # VPC (using default)
 data "aws_vpc" "default" {
@@ -57,6 +73,45 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
+# RDS Security Group
+resource "aws_security_group" "rds_sg" {
+  name        = "rds_sg"
+  description = "Security group for RDS instance"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.app_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# RDS Instance
+resource "aws_db_instance" "app_db" {
+  engine            = "postgres"
+  engine_version    = "16.3"
+  instance_class    = "db.t3.micro"
+  allocated_storage = 20
+  storage_type      = "gp2"
+  
+  identifier        = "app-db"
+  db_name           = var.db_name
+  username          = var.db_username
+  password          = var.db_password
+
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  publicly_accessible    = false
+  skip_final_snapshot    = true
+}
+
 # EC2 Instance
 resource "aws_instance" "app_server" {
   ami           = "ami-07d9cf938edb0739b"  
@@ -75,10 +130,17 @@ resource "aws_instance" "app_server" {
               sudo yum update -y
               sudo yum install -y python3 python3-pip git nginx
         
+              # Set environment variables
+              echo "export DB_USER=${var.db_username}" >> /etc/environment
+              echo "export DB_PASSWORD=${var.db_password}" >> /etc/environment
+              echo "export DB_HOST=${aws_db_instance.app_db.endpoint}" >> /etc/environment
+              echo "export DB_NAME=${var.db_name}" >> /etc/environment
+
               # Clone github repo
               
               git clone https://github.com/rblcoder/expense_tracker.git
               cd expense_tracker
+              git checkout postgrsql
               pip install -r requirements.txt
               sudo nohup uvicorn main:app --host 0.0.0.0 --port 8000 &
                # Configure Nginx
