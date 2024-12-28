f28ec2d8a53bbffc5574b672651e343bc912c1a1
Rupa Lahiri <35212670+rblcoder@users.noreply.github.com>
2024-12-28 11:29:56 +0000

    use postgresql db

--- main.py
+++ main.py
@@ -14,8 +14,14 @@ import pandas as pd
 import os
 
 
-SQLALCHEMY_DATABASE_URL ="sqlite:///./expenses.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Database connection
+DB_USER = os.getenv("DB_USER", "dbuser")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME", "expensedb")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
@@ -45,7 +51,7 @@ class Expense(ExpenseBase):
     id: int
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 # Get db session
 def get_db():
